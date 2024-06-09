75c7c8e7b38cd1c7c952171f69a899dc35cba59a
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-06-09 19:22:03 +0200

    add:footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="m-2"> 
+<footer class="mt-5"> 
     <section class="d-flex p-3 container"> 
 <div class="col-6 justify-content-between">
     <h3>Boolando S.r.l</h3>
@@ -8,15 +8,15 @@
         <li> <a href="">Diritto di recesso</a> </li>
     </ul>
 </div>
-<div class="col-6">
+<div class="col-6 d-flex flex-nowrap gap-3">
     <h4>Trovaci anche su</h4>
-    <ul>
-        <li> <a href=""><i class="fa-brands fa-twitter"></i></a></li>
+    <ul class="d-flex flex-nowrap gap-3">
+        <li> <a href=""><i class="fa-brands fa-square-twitter"></i></a></li>
         <li> <a href=""><i class="fa-brands fa-square-facebook"></i></a></li>
         <li> <a href=""><i class="fa-brands fa-square-instagram"></i></a> </li>
         <li> <a href=""><i class="fa-brands fa-square-pinterest"></i></a></li>
         <li> <a href=""><i class="fa-brands fa-youtube"></i></a> </li>
-        <li> <a href=""><i class="fa-brands fa-twitter""></i></a></li>
+    
     </ul>
 </div>
 </section>
